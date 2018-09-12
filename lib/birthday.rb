c1607d20e5386b32053d5700b742b98813dc426f
Yana <baiyang.c@gmail.com>
2018-09-12 19:58:46 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,11 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  
+  birthday_kids.each do |name_str, age_int|
+    puts "Happy Birthday #{name_str}!  You are now #{age} years old!"
+  end 
+  
 end
 
 
